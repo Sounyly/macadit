f5afc7a57c832afb3877fb5415b088927824a310
Sounyly <sounyboy29@gmail.com>
2017-07-27 15:44:57 +0200

    update readme = test

--- class/logiciel.php
+++ class/logiciel.php
@@ -47,8 +47,9 @@ class Logiciel
 	 	}
 	 	foreach($donnees as $key => $value)
 	 	{
+	 		// On récupère le nom du setter correspondant à l'attribut.
 	 		$method = 'set' .ucfirst($key);
-
+	 		 // Si le setter correspondant existe.
 	 		if(method_exists($this, $method))
 	 		{
 	 			// On appelle le setter.
